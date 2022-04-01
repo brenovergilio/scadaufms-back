51c187a8dfe1c48209a27d3f81a6e34359bb446f
Breno Vergilio <bjvergilio47@gmail.com>
2022-04-01 09:13:05 -0400

    add most of getMeasurements use cases

---
+++ src/usecases/GetPotenciasReativasFromMD30.ts
@@ -0,0 +1,18 @@
+import MedicaoMD30 from "@src/entities/MedicaoMD30";
+import MedidorMD30 from "@src/entities/MedidorMD30";
+import MedidorMD30Repository from "./repositories/MedidorMD30Repository";
+
+export default class GetPotenciasReativasFromMD30 {
+  medidorMD30Repository: MedidorMD30Repository;
+
+  constructor(medidorMD30Repository: MedidorMD30Repository) {
+    this.medidorMD30Repository = medidorMD30Repository;
+  }
+
+  execute(ip: string): Array<MedicaoMD30> {
+    const medidorMD30: MedidorMD30 = this.medidorMD30Repository.getMedidorMD30ByIP(ip);
+    const potenciasReativasWithTimestamp: Array<MedicaoMD30> = medidorMD30.getPotenciasReativasWithTimestamps();
+    
+    return potenciasReativasWithTimestamp;
+  }
+}
