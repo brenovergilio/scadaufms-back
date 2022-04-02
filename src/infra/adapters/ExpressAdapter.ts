db041567e3f1e882b24e4a08239a05df656ff3c4
Breno Vergilio <bjvergilio47@gmail.com>
2022-04-02 14:39:40 -0400

    added express adapter

---
+++ src/infra/adapters/ExpressAdapter.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express"
+
+export default class ExpressAdapter {
+  static create(fn: unknown) {
+    return async (req: Request, res: Response) => {
+      if(fn instanceof Function) {
+        const obj = fn(req.params, req.body);
+        res.json(obj);
+      }
+    }
+  }
+}
