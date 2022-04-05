db722822459dd3122547441b56be0833bde09bbd
Breno Vergilio <bjvergilio47@gmail.com>
2022-04-05 00:21:57 -0400

    added initial postgres schema

--- src/external/database/postgres/database.ts
+++ src/external/database/postgres/database.ts
@@ -11,4 +11,63 @@ const db = pgp({
   idleTimeoutMillis: 100,
 });
 
+(async () => {
+  await db.none("SET TIMEZONE='Brazil/West';");
+  await db.none(`CREATE TABLE IF NOT EXISTS medidores (
+    ip TEXT NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP,
+    nome TEXT NOT NULL,
+    porta INTEGER NOT NULL,
+    hora_ponta INTEGER NOT NULL,
+    minuto_ponta INTEGER NOT NULL,
+    intervalo_ponta INTEGER NOT NULL
+  );`)
+  await db.none(`CREATE TABLE IF NOT EXISTS medicoes (
+    medidor_ip TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    tensao_fase_a REAL NOT NULL,
+    tensao_fase_b REAL NOT NULL,
+    tensao_fase_c REAL NOT NULL,
+    corrente_fase_a REAL NOT NULL,
+    corrente_fase_b REAL NOT NULL,
+    corrente_fase_c REAL NOT NULL,
+    potencia_ativa_a REAL NOT NULL,
+    potencia_ativa_b REAL NOT NULL,
+    potencia_ativa_c REAL NOT NULL,
+    potencia_ativa_total REAL NOT NULL,
+    potencia_reativa_a REAL NOT NULL,
+    potencia_reativa_b REAL NOT NULL,
+    potencia_reativa_c REAL NOT NULL,
+    potencia_reativa_total REAL NOT NULL,
+    potencia_aparente_a REAL NOT NULL,
+    potencia_aparente_b REAL NOT NULL,
+    potencia_aparente_c REAL NOT NULL,
+    potencia_aparente_total REAL NOT NULL,
+    fator_potencia_a REAL NOT NULL,
+    fator_potencia_b REAL NOT NULL,
+    fator_potencia_c REAL NOT NULL,
+    fator_potencia_total REAL NOT NULL,
+    FOREIGN KEY (medidor_ip) REFERENCES medidores(ip) ON DELETE CASCADE ON UPDATE CASCADE
+  );`);
+  await db.none(`CREATE TABLE IF NOT EXISTS demandas (
+    medidor_ip TEXT NOT NULL,
+    timestamps TIMESTAMP NOT NULL,
+    demanda_fora_ponta REAL NOT NULL,
+    demanda_ponta REAL NOT NULL,
+    FOREIGN KEY (medidor_ip) REFERENCES medidores(ip) ON DELETE CASCADE ON UPDATE CASCADE
+  );`);
+  await db.none(`CREATE TABLE IF NOT EXISTS feriados (
+    id SMALLSERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    dia DATE NOT NULL
+  );`);
+  await db.none(`CREATE TABLE IF NOT EXISTS alarmes (
+    id SMALLSERIAL PRIMARY KEY,
+    medidor_ip TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    message TEXT NOT NULL,
+    FOREIGN KEY (medidor_ip) REFERENCES medidores(ip) ON DELETE CASCADE ON UPDATE CASCADE
+  );`);
+})();
+
 export default db;
