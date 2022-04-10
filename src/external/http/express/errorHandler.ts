90344e9a8209f5a9c2f4dfaed334e91dc9fea289
Breno Vergilio <bjvergilio47@gmail.com>
2022-04-10 13:29:13 -0400

    fix http statuscode return

--- src/external/http/express/errorHandler.ts
+++ src/external/http/express/errorHandler.ts
@@ -15,22 +15,29 @@ export default function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  if (
-    error instanceof
-    (EmptyNameError ||
-      EmptyMessageError ||
-      InvalidIPv4Error ||
-      InvalidPortError ||
-      InvalidRushError ||
-      InvalidDateRangeError)
-  )
-    return res.status(400).send(error.message);
+  if (error instanceof EmptyNameError)
+    return res.status(400).json({ statusCode: 400, message: error.message });
 
-  if (error instanceof (DuplicatedNameError || AlreadyExistsError))
-    return res.status(409).send(error.message);
+  if (error instanceof InvalidIPv4Error)
+    return res.status(400).json({ statusCode: 400, message: error.message });
+
+  if (error instanceof InvalidPortError)
+    return res.status(400).json({ statusCode: 400, message: error.message });
+
+  if (error instanceof InvalidDateRangeError)
+    return res.status(400).json({ statusCode: 400, message: error.message });
+
+  if (error instanceof InvalidRushError)
+    return res.status(400).json({ statusCode: 400, message: error.message });
 
   if (error instanceof NotFoundError)
-    return res.status(404).send(error.message);
+    return res.status(404).json({ statusCode: 404, message: error.message });
 
-  return res.status(500).send(error.message);
+  if (error instanceof DuplicatedNameError)
+    return res.status(409).json({ statusCode: 409, message: error.message });
+
+  if (error instanceof AlreadyExistsError)
+    return res.status(409).json({ statusCode: 409, message: error.message });
+
+  return res.status(500).json({ statusCode: 500, message: error.message });
 }
